a56b468844e2829c062ede66b83116b8ef19ed49
ryo.watanabe <naberyo34@gmail.com>
2020-08-03 21:56:28 +0900

    add: 最低限のview実装

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import path from 'path';
+const app = express();
+const port = 3000;
+
+app
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'pug')
+  .get('/', (_req, res) => {
+    res.render('index');
+  });
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`);
+});
