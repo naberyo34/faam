2dc608351a32591ec6494cf63204a62f7cc2ad94
ryo.watanabe <naberyo34@gmail.com>
2020-08-03 22:46:10 +0900

    fix: ポート番号修正

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 app
   .set('views', path.join(__dirname, 'views'))
