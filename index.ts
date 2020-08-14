4f743a5deb38718a8c2335ad6eb2cdfafdb6608b
ryo.watanabe <naberyo34@gmail.com>
2020-08-14 22:40:29 +0900

    add: productionのDBにもつながるようになった気がする

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import express, { response } from 'express';
-import path from 'path';
+import express from 'express';
 import mysql from 'mysql';
 
 // Expressサーバーの用意
@@ -8,10 +7,10 @@ const port = process.env.PORT || 5000;
 
 // MySQLに接続
 const mySqlConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'faam_db',
+  host: process.env.DB_HOSTNAME || 'localhost',
+  user: process.env.DB_USERNAME || 'root',
+  password: process.env.PASSWORD || '',
+  database: process.env.DB_NAME || 'faam_db',
 });
 
 mySqlConnection.connect((err) => {
