80a8fecfbee26b5609ce2e8b7b91823ffc8f04fb
ryo.watanabe <naberyo34@gmail.com>
2020-08-14 21:42:36 +0900

    add: ローカルのmySQLとつながるようになった

--- index.ts
+++ index.ts
@@ -1,14 +1,30 @@
-import express from 'express';
+import express, { response } from 'express';
 import path from 'path';
+import mysql from 'mysql';
+
+// Expressサーバーの用意
 const app = express();
 const port = process.env.PORT || 5000;
 
-app
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'pug')
-  .get('/', (_req, res) => {
-    res.render('index');
+// MySQLに接続
+const mySqlConnection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'faam_db',
+});
+
+mySqlConnection.connect((err) => {
+  if (err) throw err;
+  console.log('MySQL connected.');
+});
+
+app.get('/', (_req, res) => {
+  mySqlConnection.query('select * from users', (err, result) => {
+    if (err) throw err;
+    res.send(result);
   });
+});
 
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
