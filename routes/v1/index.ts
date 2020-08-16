28e176afc81d16187636ef2a72fc8a415e6884cb
ryo.watanabe <naberyo34@gmail.com>
2020-08-16 21:00:49 +0900

    add: postsの土台作成

--- routes/v1/index.ts
+++ routes/v1/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
-import users from './users';
+import posts from './posts';
 
 const router = express.Router();
 
 // さらに細かくルーティングしていく
-router.use('/users', users);
+router.use('/posts', posts);
 
 export default router;
