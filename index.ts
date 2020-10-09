d26c11fe0303737af2f5f3a27741e8b40ddc46a0
ryo.watanabe <ryo.watanabe@nijibox.co.jp>
2020-10-09 14:56:30 +0900

    add: パッケージ更新

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import mysql from 'mysql';
+import mongoose = require('monngoose');
 import router from './routes/v1';
 
 // Expressサーバーの用意
@@ -12,15 +12,6 @@ export const corsOptions = {
   methods: 'GET, PUT',
   optionsSuccessStatus: 200,
 };
-
-// MySQLに接続
-const settings = {
-  host: process.env.DB_HOSTNAME || 'localhost',
-  user: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'faam_db',
-};
-export const pool = mysql.createPool(settings);
 
 app.get('/', (_req, res) => {
   res.send('Hello Express.');
